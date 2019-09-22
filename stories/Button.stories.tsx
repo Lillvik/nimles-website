7d38d60512dc857d0744c1995c19c51562dd0f40
Margareta <margaretagyorgy@gmail.com>
2019-09-22 18:22:59 +0200

    Changes comment in Button.stories

--- stories/Button.stories.tsx
+++ stories/Button.stories.tsx
@@ -4,13 +4,13 @@ import { Button, ButtonProps } from '../src/components';
 
 const ButtonStory = (props: ButtonProps) =>
   <div style={{display: 'flex'}}>
-    <div style={{padding: 10}}><Button {...props}>{props.children}</Button></div> //här finns themes på props
+    <div style={{padding: 10}}><Button {...props}>{props.children}</Button></div> //themes is on props.
     <div style={{padding: 10}}><Button border {...props}>{props.children} with Border</Button></div>
     <div style={{padding: 10}}><Button raised {...props}>Raised {props.children}</Button></div>
   </div>;
 
 storiesOf('Button', module)
   .add('Default', () => <ButtonStory>Default</ButtonStory>)
-  .add('Primary', () => <ButtonStory primary>Primary</ButtonStory>)
+  .add('Primary', () => <ButtonStory primary>Primary</ButtonStory>) 
   .add('Secondary', () => <ButtonStory secondary>Secondary</ButtonStory>);
 
