c4b14203216a3eadb9a270289216450aaa4ad45b
Andreas Lillvik <andreas@cloudtechnologies.se>
2019-09-22 08:49:51 +0200

    Adds modal component

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,10 +1,13 @@
 import styled from "@emotion/styled";
 import { Theme } from "../theme";
-import React from "react";
-import { Card } from "./Card";
+import React, { MouseEvent } from "react";
+import Close from "./svg/Close";
 
 export type ModalProps = {
   theme?: Theme;
+  onClose: Function;
+  children: any;
+  open: boolean;
 };
 
 const ModalContainer = styled.div`
@@ -20,18 +23,87 @@ const ModalContainer = styled.div`
   z-index: 900;
 `;
 
-const ModalBackdrop = styled.div``;
-
-const ModalCard = styled(Card)`
+const ModalBackdrop = styled.div`
+  position: fixed;
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  overflow: auto;
+  background-color: rgba(0, 0, 0, 0.8);
+  z-index: 998;
 `;
 
-export const Modal = (props: ModalProps) => {
-  return (
-    <ModalContainer>
-      <ModalBackdrop />
-      <ModalCard>
+const ModalBox = styled.div`
+  position: absolute;
+  left: 0;
+  top: 80px;
+  right: 0;
+  bottom: 0;
+  background-color: #fefefe;
+  border-radius: 4px;
+  box-shadow: 1px 1px 5px rgba(157, 157, 157, 0.4);
+  z-index: 9999999999;
 
-      </ModalCard>
+  @media (min-width: 768px) {
+    & {
+      position: relative;
+      width: 90%;
+      max-width: 500px;
+    }
+  }
+`;
+
+const CloseButton = styled.button`
+  color: #aaa;
+  position: absolute;
+  background: transparent;
+  border: none;
+  top: 0;
+  right: 0;
+  padding: 10px;
+  width: 40px;
+  height: 40px;
+  outline: none;
+`;
+
+export const ModalBody = styled.div`
+  padding: 30px;
+`;
+
+const ModalClose = styled(Close)`
+  height: 20px;
+  width: 20px;
+  & path {
+    fill: #aaa;
+  }
+  &:hover,
+  &:focus {
+    color: black;
+    text-decoration: none;
+    cursor: pointer;
+  }
+`;
+
+export const Modal = ({ children, open, onClose }: ModalProps) => {
+  const handleClose = (e: MouseEvent) => {
+    console.log("handleClose");
+    e.preventDefault();
+    console.log(onClose);
+    onClose();
+  };
+
+  console.log(open);
+  
+  return open ? (
+    <ModalContainer>
+      <ModalBackdrop onClick={handleClose} />
+      <ModalBox>
+        <CloseButton onClick={handleClose}>
+          <ModalClose />
+        </CloseButton>
+        {children}
+      </ModalBox>
     </ModalContainer>
-  );
+  ) : null;
 };
--- stories/Modal.stories.tsx
+++ stories/Modal.stories.tsx
@@ -1,6 +1,26 @@
-import * as React from 'react';
-import { storiesOf } from '@storybook/react';
-import { Template } from '../src/components';
+import * as React from "react";
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+import { withKnobs, boolean } from "@storybook/addon-knobs";
+import { Modal, ModalBody, Container, Button } from "../src/components";
 
-storiesOf('Template', module)
-  .add('Default', () => <Template>Default Template</Template>);
+storiesOf("Modal", module)
+  .addDecorator(withKnobs)
+  .add("Default", () => {
+    let open = true;
+
+    const toggle = () => {
+      console.log("toggle");
+      action("toggle")();
+      open = !open;
+    };
+
+    return (
+      <Container>
+        <Button onClick={toggle}>Open Modal</Button>
+        <Modal open={boolean('Open', true)} onClose={toggle}>
+          <ModalBody>Default Modal</ModalBody>
+        </Modal>
+      </Container>
+    );
+  });
